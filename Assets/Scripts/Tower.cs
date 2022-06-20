946fb5a3b7d0623a9fb9e4aba63c74bdc6ccfe69
LAPTOP-58QGQQLE\David Private <jonid89@gmail.com>
2022-06-20 12:21:44 +0200

    Queue of enemies within range

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -7,10 +7,11 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] private float fireRate = 4f;
-
-    private GameObject enemy;    
     private float cooldown;
     ObjectPooler objectPooler;
+
+    private List<Enemy> enemies = new List<Enemy>();
+    private Enemy target;
 
     void Start()
     {
@@ -20,28 +21,51 @@ public class Tower : MonoBehaviour
 
     void Update()
     {
+        GetCurrentTarget();
         cooldown -= Time.deltaTime;
-        if(cooldown <= 0 && enemy != null){
+        if(cooldown <= 0 && target != null){
             createProjectile();
             cooldown = fireRate;
         }
     }
 
     void createProjectile(){
-        GameObject projectile = objectPooler.SpawnFromPool("Projectile",transform.position,Quaternion.identity, this.transform.parent, enemy.transform.position);
+        GameObject projectile = objectPooler.SpawnFromPool("Projectile",transform.position,Quaternion.identity, this.transform.parent, target.transform.position);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(enemy == null)
+        if (other.CompareTag("Enemy"))
         {
-            enemy = other.gameObject;
+            Enemy newEnemy = other.GetComponent<Enemy>();
+            enemies.Add(newEnemy);
         }
+        Debug.Log(enemies[0]+" "+enemies[1]+" "+enemies[2]);
+        /*if(enemy == null)
+        {
+            //enemy = other.gameObject;
+        }*/
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        enemy = null;
+        Enemy enemy = other.GetComponent<Enemy>();
+        if (enemies.Contains(enemy))        
+        {
+            enemies.Remove(enemy);
+        }
+        //enemy = null;
+    }
+
+    private void GetCurrentTarget()
+    {
+        if (enemies.Count <= 0)
+        {
+            target = null;
+            return;
+        }
+        
+        target = enemies[0];
     }
 
 }
